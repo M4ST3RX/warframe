520fe371c5a37e0b1d739557ea3dfc068c69a3c9
Martin Kun <martinkun29@gmail.com>
2021-04-13 20:07:34 +0100

    Add ip nullable

--- database/migrations/2021_04_13_184042_add_ip_to_users.php
+++ database/migrations/2021_04_13_184042_add_ip_to_users.php
@@ -14,7 +14,7 @@ class AddIpToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('ip');
+            $table->string('ip')->nullable();
         });
     }
 
